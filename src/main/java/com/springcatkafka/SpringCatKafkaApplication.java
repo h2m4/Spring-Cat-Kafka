7633c61dd70c8b9e809be910a69731f98a9734a3
hiumei <hiu2mei4@gmail.com>
2022-03-07 08:22:23 -0500

    int commit

---
+++ src/main/java/com/springcatkafka/SpringCatKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.springcatkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCatKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCatKafkaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springcatkafka/SpringCatKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcatkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCatKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
